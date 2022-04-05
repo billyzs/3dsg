2692a4bc62d1ee861635f54d2fe6dce72474fd89
bzs <me@bzs.rocks>
2022-04-05 13:50:13 +0200

    add attributes and relationships parsed from 3DSSG
    - enums are parsed from 3DSSG .txt files, ordering preserved
    - explicit export enums in package
    
    (cherry picked from commit c7578bf71daac1328bcef0aa1c4d6e49e5c6e8a8)

---
+++ delta_3dsg/__init__.py
@@ -0,0 +1,9 @@
+from .Attributes3DSSG import Attributes3DSSG
+from .Relationships3DSSG import Relationships3DSSG
+from .Taxonomy3DSSG import Objects3DSSG
+
+__all__ = [
+    Attributes3DSSG,
+    Relationships3DSSG,
+    Objects3DSSG,
+]
