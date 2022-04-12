1da6dc526979dfdfafd21401a2069034c4db4597
samuel-looper <slooper@noteworthy.ai>
2022-04-12 19:05:21 +0200

    Improved logging, training, and fixed some bugs in the code

--- train.py
+++ train.py
@@ -1,65 +1,74 @@
 import torch
 from torch_geometric.loader import DataLoader
-from dataset.SceneGraphChangeDataset import SceneGraphChangeDataset
+from dataset.SceneGraphChangeDataset import SceneGraphChangeDataset, transform_locations
+from utils.extract_data import build_scene_graph
+from utils.logging import TrainingLogger
 from models.SimpleGCN import SimpleMPGNN
+from models.SimpleMLP import SimpleMLP
+from eval import calculate_conf_mat
 import numpy as np
 from tqdm import tqdm
 
 
-def train_gnn(dset, gnn):
+def calculate_training_loss(data, nnet, l_fn, nnet_type):
+    x_i = data.x
+    y_i = data.y
+    m_i = data.mask
+    if nnet_type == "gnn":
+        e_idx_i = data.edge_index.type(torch.LongTensor)
+        e_att_i = data.edge_attr
+        pred_i = nnet(x_i, e_idx_i, e_att_i)
+    else:
+        pred_i = nnet(x_i)
+    loss_1 = l_fn(pred_i[:, :2], y_i)
+    loss_2 = l_fn(pred_i[:, 2], m_i)
+    loss = torch.sum(torch.multiply(torch.sum(loss_1, 1), m_i))/torch.sum(m_i) + torch.mean(loss_2)
+    return loss, pred_i
+
+
+def train_neuralnet(dset, neuralnet, hyperparams, visualize=False):
     train_n = int(len(dset) * 0.85)
     train_set, val_set = torch.utils.data.random_split(dset,  [train_n, len(dset)-train_n])
-    # train_loader = DataLoader(train_set, batch_size=16)
-    # val_loader = DataLoader(val_set, batch_size=16)
-    l_fn = torch.nn.BCEWithLogitsLoss(reduction="none")
-    epochs = 10
-    print_interval = 250
-    loss_cumul = 0
-    train_losses = []
-    val_losses = []
-    optimizer = torch.optim.Adam(model.parameters(), lr=001, weight_decay=5e-4)
+    train_loader = DataLoader(train_set, batch_size=hyperparams["bs"])
+    val_loader = DataLoader(val_set, batch_size=1)
+    l_fn = hyperparams["loss"]
+    epochs = hyperparams["num_epochs"]
+    nnet_type = hyperparams["model_type"]
+    optimizer = torch.optim.Adam(model.parameters(), lr=hyperparams["lr"], weight_decay=hyperparams["weight_decay"])
+
+    logger = TrainingLogger(hyperparams["model_name"], print_interval=20, train_n=int(train_n/hyperparams["bs"]))
     for i in range(epochs):
-        # Training
-        print("Training. Epoch {}".format(i))
-        gnn.train()
-        iter_count = 0
-        for data in train_set:
+        print("Training. Epoch {}".format(i+1))
+        neuralnet.train()
+        for data in train_loader:
             optimizer.zero_grad()
-            x_i = data.x
-            y_i = data.y
-            m_i = data.mask
-            e_idx_i = data.edge_index.type(torch.LongTensor)
-            e_att_i = data.edge_attr
-            pred_i = gnn(x_i, e_idx_i, e_att_i)
-            loss_1 = l_fn(pred_i[:, :2], y_i)
-            loss_2 = l_fn(pred_i[:, 2], m_i)
-            loss = torch.mean(torch.multiply(torch.sum(loss_1, 1), m_i)) + torch.mean(loss_2)
+            loss, _ = calculate_training_loss(data, neuralnet, l_fn, nnet_type)
+            logger.log_training_iter(loss.item())
             loss.backward()
             optimizer.step()
+        logger.log_training_epoch(model)
 
-            if iter_count % print_interval:
-                print("Epoch {}, {}/{}. Loss = {}".format(i, iter_count, len(train_set), loss_cumul/iter_count))
-                train_losses.append(loss_cumul/iter_count)
-                loss_cumul = 0
-            loss_cumul += loss.item()
-
-        # Validation
         print("Validation:")
         model.eval()
-        val_loss = 0
-        for data in tqdm(val_set):
-            x_i = data.x
-            y_i = data.y
-            m_i = data.mask
-            e_idx_i = data.edge_index.type(torch.LongTensor)
-            e_att_i = data.edge_attr
-            pred_i = gnn(x_i, e_idx_i, e_att_i)
-            loss_1 = l_fn(pred_i[:, :2], y_i)
-            loss_2 = l_fn(pred_i[:, 2], m_i)
-            loss = torch.mean(torch.multiply(torch.sum(loss_1, 1), m_i)) + torch.mean(loss_2)
-            val_loss += loss.item()
-        print("loss = {}".format(val_loss/len(val_set)))
-        val_losses.append(val_loss / len(val_set))
+        for data in tqdm(val_loader):
+            loss, pred_i = calculate_training_loss(data, neuralnet, l_fn, nnet_type)
+            val_loss = loss.item()
+            state_conf = calculate_conf_mat(pred_i[:, 0], data.y[:, 0])
+            pos_conf = calculate_conf_mat(pred_i[:, 1], data.y[:, 1])
+            mask_conf = calculate_conf_mat(pred_i[:, 2], data.mask)
+            logger.log_validation_iter(val_loss, state_conf, pos_conf, mask_conf)
+
+            if visualize:
+                _, nodes_1, edges_1 = build_scene_graph(train_set.dataset.objects_dict, train_set.dataset.relationships_dict, data[0].input_graph)
+                _, nodes_2, edges_2 = build_scene_graph(train_set.dataset.objects_dict, train_set.dataset.relationships_dict, data[0].output_graph)
+                transf_node_1 = transform_locations(nodes_1, data.input_tf)
+                transf_node_2 = transform_locations(nodes_2, data.input_tf)
+
+        logger.log_validation_epoch()
+
+    logger.plot_training_losses()
+    logger.plot_valid_losses()
+    logger.plot_accuracies()
 
 
 if __name__ == "__main__":
@@ -68,8 +77,31 @@ if __name__ == "__main__":
     test_split = np.load("test_split.npy")
     tv_split = [val for val in range(len(dataset)) if val not in test_split]
     tv_set = dataset[tv_split]
-    model = SimpleMPGNN(tv_set.num_node_features, tv_set.num_classes + 1, tv_set.num_edge_features)
-    train_gnn(tv_set, model)
 
+    hyperparams = {
+        "model_name": "Simple GNN v1",
+        "model_type": "gnn",
+        "hidden_layers": [16],
+        "lr": 0.001,
+        "weight_decay": 5e-4,
+        "num_epochs": 10,
+        "bs": 16,
+        "loss": torch.nn.BCEWithLogitsLoss(reduction="none")
+    }
 
-    # train_set, val_set, test_set = torch.utils.data.random_split(dataset, [0.75, 0.15, 0.1])
+    model = SimpleMPGNN(tv_set.num_node_features, tv_set.num_classes + 1, tv_set.num_edge_features, hyperparams["hidden_layers"])
+    train_neuralnet(tv_set, model, hyperparams, visualize=False)
+
+    hyperparams = {
+        "model_name": "Simple MLP Baseline",
+        "model_type": "mlp",
+        "hidden_layers": [32, 32],
+        "lr": 0.01,
+        "weight_decay": 5e-4,
+        "num_epochs": 10,
+        "bs": 16,
+        "loss": torch.nn.BCEWithLogitsLoss(reduction="none")
+    }
+
+    model = SimpleMLP(tv_set.num_node_features, tv_set.num_classes + 1, hyperparams["hidden_layers"])
+    train_neuralnet(tv_set, model, hyperparams, visualize=False)
